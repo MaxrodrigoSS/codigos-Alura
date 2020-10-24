3954d967eb8f9f823a197e4523146f13eea5810a
Max Rodrigo Silva Santiago <max.rodrigo2001@gmail.com>
2020-10-24 19:06:20 -0300

    24/10/202

---
+++ JavaScript/6-lista.js
@@ -0,0 +1,11 @@
+console.log("trabalhando com listas");
+
+const listaDeLugares = new Array(
+    "São Paulo", "Salvador", "Rio De Janeiro");
+
+listaDeLugares.push("Curitiba");
+
+listaDeLugares.slice(1, 1);
+console.log(listaDeLugares);
+
+console.log(listaDeLugares[1]);
---
+++ JavaScript/7-trabalnhadoComCondicionais.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando Com Condicionais")
+
+const listaDeLugares = new Array('Salvador', 'São Paulo', "Rio De Janeiro")
+
+console.log("Destinos diponiveis.")
+console.log(listaDeLugares)
+
+if (idadeDoComprador >= 18)
