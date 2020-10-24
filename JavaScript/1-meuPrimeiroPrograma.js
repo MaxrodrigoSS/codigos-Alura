58952cc982e55fecdb94ff24d388dcfe0fd689ff
Max Rodrigo Silva Santiago <max.rodrigo2001@gmail.com>
2020-10-24 00:12:29 -0300

    JS1
    aula 2 e 3

---
+++ JavaScript/1-meuPrimeiroPrograma.js
@@ -0,0 +1,7 @@
+console.log("Meu Primeiro Programa: Trabalhando com variaveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade + 2);
+console.log(idade - 2);
---
+++ JavaScript/3-opereacoesAritimeticas.js
@@ -0,0 +1,4 @@
+console.log("Operacoes Aritimeticas");
+
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2)
